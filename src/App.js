6093f7e4940af61a9ec860a1f8ee51bc4f7bc288
Kory Stone <kory.stone10@gmail.com>
2019-11-04 13:46:38 -0600

    Added in the list of quotes. Set up the QuoteBox component to use quote props passed in. Added functionality to generate and show a new random quote

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
 import React, { Component } from 'react';
 import './App.css';
 import QuoteBox from './QuoteBox';
+import quotes from './quotes';
 
+class App extends Component { 
+  //Setting up the constructor function so we can set our default state
+  constructor(props) {
+    super(props);
+    //Set the first quote as our default state
+    this.state = {
+      quote: quotes[0].quote,
+      author: quotes[0].author
+    }
+  }
 
-class App extends Component {
+  //Create a function called getRandomQuote that returns a random quote from the array of quotes. Hint: You'll need to generate a random number between 0 and 1 less than the length of the array
+  getRandomQuote() {
+    const randomQuoteIndex = Math.floor(Math.random() * quotes.length);
+    return quotes[randomQuoteIndex];
+  }
+
+  //Function that will be called when the new quote button on the QuoteBox component is pressed
+  handleClick = () => {
+    //Generate a random quote
+    const randomQuote = this.getRandomQuote();
+    //Update our state with information from that random quote
+    this.setState({
+      quote: randomQuote.quote,
+      author: randomQuote.author
+    });
+  }
+
   render() {
+
+    console.log(quotes[0].author);
+    console.log(quotes[0].quotes);
+
     return(
       <div>
-        <QuoteBox />
+        <QuoteBox
+          quote={this.state.quote}
+          author={this.state.author} 
+          handleClick={this.handleClick}
+          />
       </div>
     );
   }
